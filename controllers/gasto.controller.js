24d72a60502cfaf85939adea09ec1362536d995b
JCGutierrezConcha <jc.gutierrez.concha@gmail.com>
2024-06-04 19:49:42 -0400

    Agrega códigos de estado de respuesta HTTP

--- controllers/gasto.controller.js
+++ controllers/gasto.controller.js
@@ -2,21 +2,27 @@ import { modelGasto } from "../models/gasto.model.js"
 
 const getGastos = async (req, res) => {
     try {
-        const gastos = await modelGasto.findAllGastos();
-        return res.json({ gastos })
+        const gastos = await modelGasto.findAllGastos()
+        return res.status(200).json({ gastos })
     } catch (error) {
         console.error("Error al procesar los archivos:", error)
+        return res.status(500).json({ ok: false, msg: "Error de servidor" })
     }
 };
 const addGasto = async (req, res) => {
     try {
         const { roommate, descripcion, monto } = req.body
+
+        if (!roommate || !descripcion || !monto)
+            return res.status(400).json({ ok: false, msg: "Todos los campos obligatorios" })
+
         const newGasto = await modelGasto.createGasto(roommate, descripcion, monto)
         await modelGasto.reparteGastos()
         await modelGasto.saldoNeto()
-        return res.json(newGasto)
+        return res.status(201).json(newGasto)
     } catch (error) {
         console.error("Error al procesar los archivos:", error)
+        return res.status(500).json({ ok: false, msg: "Error de servidor" })
     }
 }
 
@@ -26,9 +32,10 @@ const deleteGasto = async (req, res) => {
         const gasto = await modelGasto.removeGasto(id)
         await modelGasto.reparteGastos()
         await modelGasto.saldoNeto()
-        return res.json(gasto)
+        return res.status(200).json(gasto)
     } catch (error) {
         console.error("Error al procesar los archivos:", error)
+        return res.status(500).json({ ok: false, msg: "Error de servidor" })
     }
 }
 
@@ -40,9 +47,10 @@ const updateGasto = async (req, res) => {
         const gasto = await modelGasto.actualiceGasto(id, roommate, descripcion, monto)
         await modelGasto.reparteGastos()
         await modelGasto.saldoNeto()
-        return res.json(gasto)
+        return res.status(200).json(gasto)
     } catch (error) {
         console.error("Error al procesar los archivos:", error)
+        return res.status(500).json({ ok: false, msg: "Error de servidor" })
     }
 }
 
--- controllers/roommate.controller.js
+++ controllers/roommate.controller.js
@@ -3,18 +3,20 @@ import { modelRoommate } from "../models/roommate.model.js"
 const getRoommates = async (req, res) => {
     try {
         const roommates = await modelRoommate.findAllRoommates()
-        return res.json({ roommates })
+        return res.status(200).json({ roommates })
     } catch (error) {
         console.error("Error al procesar los archivos:", error)
+        return res.status(500).json({ ok: false, msg: "Error de servidor" })
     }
 }
 
 const addRoommate = async (req, res) => {
     try {
         const roommate = await modelRoommate.createRoommate()
-        return res.json(roommate)
+        return res.status(201).json(roommate)
     } catch (error) {
         console.error("Error al procesar los archivos:", error)
+        return res.status(500).json({ ok: false, msg: "Error de servidor" })
     }
 }
 
